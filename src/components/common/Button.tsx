390abc5259700be13beaac0412cb311680d2c526
minjilee <budlalswl1129@gmail.com>
2024-06-24 17:22:50 +0900

    design: button 그림자 추가, topbar 컴포넌트 생성

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -9,7 +9,7 @@ export const Button: FC<IProps> = ({ message, onClick }) => {
   return (
     <div className='absolute w-full flex justify-center bottom-6'>
       <button
-        className='w-[90%] bg-hanaNavGreen rounded-2xl py-4 text-xl font-hanaBold text-white'
+        className='w-[90%] bg-hanaNavGreen rounded-2xl py-4 text-xl font-hanaBold text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.25)]'
         onClick={onClick}
       >
         {message}
