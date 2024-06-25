6da158a3d0b853029a4a4f8d5c7c945ce6ab56b5
seoyeon08 <silversun0808@gmail.com>
2024-06-25 14:07:21 +0900

    design: keypad 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Keypad from './components/common/Keypad';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,6 +16,7 @@ function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <Outlet />
+        <Keypad />
       </QueryClientProvider>
     </>
   );
