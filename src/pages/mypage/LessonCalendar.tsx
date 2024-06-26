cfcd55305927e2a4ecb8e2bdbedeeea484ad3438
seoyeon08 <silversun0808@gmail.com>
2024-06-26 10:07:14 +0900

    design: calendar css 구현

--- src/pages/mypage/LessonCalendar.tsx
+++ src/pages/mypage/LessonCalendar.tsx
@@ -3,23 +3,54 @@ import { Navbar } from '../../components/common/Navbar';
 import { Topbar } from '../../components/common/Topbar';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // react-calendar 기본 스타일
+import CalendarCss from '../css/Calendar.module.css';
 import moment from 'moment';
 
 export const LessonCalendar = () => {
-  //캘린더에서 년, 월을 변수에 담아줘야 함
   const navigate = useNavigate();
-  const [value, onChange] = useState(new Date());
-  const handleDateChange = () => {};
+  const [value, onChange] = useState(new Date()); // 초깃값 : 현재 날짜
+  const [mark, setMark] = useState(['2024-06-07']);
+
+  const handleDateChange = (date: any) => {
+    onChange(date);
+  };
+
+  const handleOpenList = () => {};
+
   return (
     <div>
       <Topbar title='신청 클래스 일정' onClick={() => navigate('/mypage')} />
-      <div className='flex items-center justify-center'>
+      <div className='flex items-center justify-center font-hanaRegular'>
         <Calendar
-          className='w-[334px] h-[275px] mt-3 p-5 text-center font-hanaRegular bg-white rounded-2xl border-[1px] border-hanaSilver'
-          formatDay={(locale, date) => moment(date).format('DD')}
+          className='mt-3 p-5 text-center bg-white rounded-2xl border-[1px] border-hanaSilver'
+          formatDay={(locale, date) => moment(date).format('D')}
+          locale='ko'
+          next2Label={null}
+          prev2Label={null}
           onChange={handleDateChange}
+          showNeighboringMonth={false}
           value={value}
-        ></Calendar>
+          calendarType='gregory'
+          onClickDay={handleOpenList}
+          tileContent={({ date, view }) => {
+            // 날짜 타일에 컨텐츠 추가하기 (html 태그)
+            // 추가할 html 태그를 변수 초기화
+            let html = [];
+            // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
+            if (mark.find((x) => x === moment(date).format('YYYY-MM-DD'))) {
+              html.push(<div className='bg-pink-50'>ㅇ</div>);
+            }
+            // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
+            return (
+              <>
+                <div className='flex justify-center items-center absoluteDiv'>
+                  {html}
+                </div>
+              </>
+            );
+          }}
+        />
       </div>
       <div className='m-5'>
         <p className='font-hanaMedium text-xl ml-1'>나의 일정 모아보기</p>
