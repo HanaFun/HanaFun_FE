6abdb98bca687f168b16efb2d568610be11a6143
minjilee <budlalswl1129@gmail.com>
2024-06-29 00:42:56 +0900

    chore: 클래스 검색 리스트 크기 조정

--- src/components/molecules/LessonSearchCard.tsx
+++ src/components/molecules/LessonSearchCard.tsx
@@ -18,13 +18,13 @@ export const LessonSearchCard: FC<IProps> = ({ lesson }) => {
 
   return (
     <div
-      className='bg-white flex justify-between gap-x-2 p-3 rounded-2xl cursor-pointer drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
+      className='bg-white flex justify-between p-3 rounded-2xl cursor-pointer drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]'
       onClick={() => navigate(`/lesson/${lesson.lesson_id}`)}
     >
       <div className='w-36 h-36 rounded-xl overflow-hidden object-fill'>
         <img src={lesson.image} alt='lesson_image' className='w-full h-full' />
       </div>
-      <div className='w-2/3 flex flex-col justify-between px-2'>
+      <div className='w-44 flex flex-col justify-between px-2'>
         <div>
           <h1 className='w-full font-hanaRegular text-base mb-1 break-all'>
             {lesson.title}
